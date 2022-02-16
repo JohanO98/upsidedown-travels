e740d43f573823b0ae419070fefcf5dd89a9b5d9
Tord-Kodehode <89379448+Tord-Kodehode@users.noreply.github.com>
2022-02-16 11:10:37 +0100

    test Tord 1 man

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. WooW!
+          Edit <code>src/App.js</code> and save to reload. WooW Man!
         </p>
         <a
           className="App-link"
